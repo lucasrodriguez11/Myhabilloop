50b037d80eece58fb02b29f15aa89e0f0af81327
Martin-Rodriguez7 <lmartin.rodriguez99@gmail.com>
2023-05-23 19:25:26 -0300

    activity
    se agrego para que el usuario se pueda registrar y guarda en la base de datos y redirige a la pantalla de home

--- app/src/main/java/com/internet/habiloop/activities/RegisterActivity.java
+++ app/src/main/java/com/internet/habiloop/activities/RegisterActivity.java
@@ -1,16 +1,122 @@
 package com.internet.habiloop.activities;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.AlertDialog;
+import android.app.ProgressDialog;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.internet.habiloop.R;
 
+import java.security.AuthProvider;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class RegisterActivity extends AppCompatActivity {
+
+    EditText username, fullname, email, password,passwordconfir, phone;
+    Button mButtonRegister;
+    FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+
+        username = findViewById(R.id.usernamereg);
+        email = findViewById(R.id.txtInRegEmail);
+        phone = findViewById(R.id.numtelreg);
+        fullname = findViewById(R.id.fullnamereg);
+        password = findViewById(R.id.txtInPasswordReg);
+        passwordconfir = findViewById(R.id.txtInConfirPasword);
+
+        mButtonRegister = findViewById(R.id.btnRegister);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        mButtonRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                register();
+            }
+        });
+
+        /*mCircleImageViewBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });*/
     }
+
+    private void register() {
+        String Tusername = username.getText().toString();
+        String Temail = email.getText().toString();
+        String Tpassword  = password.getText().toString();
+        String TconfirmPassword = passwordconfir.getText().toString();
+
+        if (!Tusername.isEmpty() && !Temail.isEmpty() && !Tpassword.isEmpty() && !TconfirmPassword.isEmpty()) {
+            if (isEmailValid(Temail)) {
+                if (Tpassword.equals(TconfirmPassword)) {
+                    if (Tpassword.length() >= 6) {
+                        createUser(Temail, Tpassword);
+                    }
+                    else {
+                        Toast.makeText(this, "La contraseña debe tener al menos 6 caracteres", Toast.LENGTH_SHORT).show();
+                    }
+                }
+                else {
+                    Toast.makeText(this, "Las contraseña no coinciden", Toast.LENGTH_SHORT).show();
+                }
+            }
+            else {
+                Toast.makeText(this, "Insertaste todos los campos pero el correo no es valido", Toast.LENGTH_LONG).show();
+            }
+        }
+        else {
+            Toast.makeText(this, "Para continuar inserta todos los campos", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    private void createUser(String email, String password) {
+        mAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful()) {
+                    Toast.makeText(RegisterActivity.this, "El usuario se registro correctamente", Toast.LENGTH_SHORT).show();
+                    startActivity(new Intent(RegisterActivity.this, HomeActivity.class));
+                }
+                else {
+                    Toast.makeText(RegisterActivity.this, "No se pudo registrar el usuario", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+
+    /*
+     * VERIFICAR QUE SEA UN EMAIL VALIDO
+     */
+    public boolean isEmailValid(String email) {
+        String expression = "^[\\w\\.-]+@([\\w\\-]+\\.)+[A-Z]{2,4}$";
+        Pattern pattern = Pattern.compile(expression, Pattern.CASE_INSENSITIVE);
+        Matcher matcher = pattern.matcher(email);
+        return matcher.matches();
+    }
+
 }
