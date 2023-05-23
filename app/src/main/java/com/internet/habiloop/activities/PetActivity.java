0e399986fdf7a25e4aabe39342f8f9f7bbf06895
Martin-Rodriguez7 <lmartin.rodriguez99@gmail.com>
2023-05-22 22:27:15 -0300

    se agrega pet activity

---
+++ app/src/main/java/com/internet/habiloop/activities/PetActivity.java
@@ -0,0 +1,16 @@
+package com.internet.habiloop.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.internet.habiloop.R;
+
+public class PetActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pet);
+    }
+}
