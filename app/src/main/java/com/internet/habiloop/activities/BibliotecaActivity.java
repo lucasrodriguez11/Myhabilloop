9094b1a2d730c18f76560b5d426ee6f5896d7cc6
Lucas <rodriguezlucas011@gmail.com>
2023-05-23 21:01:16 -0300

    se agrego pantalla de biblioteca de habitos y redireccionamiento

---
+++ app/src/main/java/com/internet/habiloop/activities/BibliotecaActivity.java
@@ -0,0 +1,22 @@
+package com.internet.habiloop.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.internet.habiloop.R;
+
+public class BibliotecaActivity extends AppCompatActivity {
+
+
+
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bibliotecadehabitos);
+    }
+
+}
